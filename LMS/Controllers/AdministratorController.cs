9353ce76ff5ee38fc36a6fef13357bb4001ed409
Justin Perez <u1248103@umail.utah.edu>
2023-04-16 10:44:55 -0600

    pushing to github

---
+++ LMS/Controllers/AdministratorController.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using LMS.Models.LMSModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LMS.Controllers
+{
+    public class AdministratorController : Controller
+    {
+        private readonly LMSContext db;
+
+        public AdministratorController(LMSContext _db)
+        {
+            db = _db;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Department(string subject)
+        {
+            ViewData["subject"] = subject;
+            return View();
+        }
+
+        public IActionResult Course(string subject, string num)
+        {
+            ViewData["subject"] = subject;
+            ViewData["num"] = num;
+            return View();
+        }
+
+        /*******Begin code to modify********/
+
+        /// <summary>
+        /// Create a department which is uniquely identified by it's subject code
+        /// </summary>
+        /// <param name="subject">the subject code</param>
+        /// <param name="name">the full name of the department</param>
+        /// <returns>A JSON object containing {success = true/false}.
+        /// false if the department already exists, true otherwise.</returns>
+        public IActionResult CreateDepartment(string subject, string name)
+        {
+            var departments =
+                from department in db.Departments
+                select department.Subject;
+
+            if (departments.Contains(subject))
+            {
+                return Json(new { success = false });
+            }
+            else
+            {
+                var newDepartment = new Department();
+                newDepartment.Subject = subject;
+                newDepartment.Name = name;
+                db.Departments.Add(newDepartment);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+
+        }
+
+
+        /// <summary>
+        /// Returns a JSON array of all the courses in the given department.
+        /// Each object in the array should have the following fields:
+        /// "number" - The course number (as in 5530)
+        /// "name" - The course name (as in "Database Systems")
+        /// </summary>
+        /// <param name="subjCode">The department subject abbreviation (as in "CS")</param>
+        /// <returns>The JSON result</returns>
+        public IActionResult GetCourses(string subject)
+        {
+            var allCourses =
+                from courses in db.Courses
+                where courses.Department == subject
+                select new { number = courses.Number, name = courses.Name };
+
+            return Json(allCourses.ToArray());
+        }
+
+        /// <summary>
+        /// Returns a JSON array of all the professors working in a given department.
+        /// Each object in the array should have the following fields:
+        /// "lname" - The professor's last name
+        /// "fname" - The professor's first name
+        /// "uid" - The professor's uid
+        /// </summary>
+        /// <param name="subject">The department subject abbreviation</param>
+        /// <returns>The JSON result</returns>
+        public IActionResult GetProfessors(string subject)
+        {
+            var professorsInSubject =
+                from professors in db.Professors
+                where professors.WorksIn == subject
+                select new { lname = professors.LName, fname = professors.FName, uid = professors.UId };
+
+            return Json(professorsInSubject.ToArray());
+
+        }
+
+
+
+        /// <summary>
+        /// Creates a course.
+        /// A course is uniquely identified by its number + the subject to which it belongs
+        /// </summary>
+        /// <param name="subject">The subject abbreviation for the department in which the course will be added</param>
+        /// <param name="number">The course number</param>
+        /// <param name="name">The course name</param>
+        /// <returns>A JSON object containing {success = true/false}.
+        /// false if the course already exists, true otherwise.</returns>
+        public IActionResult CreateCourse(string subject, int number, string name)
+        {
+            var allCourses =
+                from course in db.Courses
+                select course;
+            bool exists = allCourses.Any(course => course.Department == subject && course.Number == number);
+
+            if (exists)
+            {
+                return Json(new { success = false });
+            }
+            else
+            {
+                var newCourse = new Course();
+                newCourse.Number = (uint)number;
+                newCourse.Name = name;
+                newCourse.Department = subject;
+                db.Courses.Add(newCourse);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+        }
+
+
+
+        /// <summary>
+        /// Creates a class offering of a given course.
+        /// </summary>
+        /// <param name="subject">The department subject abbreviation</param>
+        /// <param name="number">The course number</param>
+        /// <param name="season">The season part of the semester</param>
+        /// <param name="year">The year part of the semester</param>
+        /// <param name="start">The start time</param>
+        /// <param name="end">The end time</param>
+        /// <param name="location">The location</param>
+        /// <param name="instructor">The uid of the professor</param>
+        /// <returns>A JSON object containing {success = true/false}. 
+        /// false if another class occupies the same location during any time 
+        /// within the start-end range in the same semester, or if there is already
+        /// a Class offering of the same Course in the same Semester,
+        /// true otherwise.</returns>
+        public IActionResult CreateClass(string subject, int number, string season, int year, DateTime start, DateTime end, string location, string instructor)
+        {
+            var allClasses =
+                from classes in db.Classes
+                select classes;
+
+         
+            return Json(new { success = false});
+        }
+
+
+        /*******End code to modify********/
+
+    }
+}
+
---
+++ LMS/Controllers/CommonController.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using LMS.Models.LMSModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LMS.Controllers
+{
+    public class CommonController : Controller
+    {
+        private readonly LMSContext db;
+
+        public CommonController(LMSContext _db)
+        {
+            db = _db;
+        }
+
+        /*******Begin code to modify********/
+
+        /// <summary>
+        /// Retreive a JSON array of all departments from the database.
+        /// Each object in the array should have a field called "name" and "subject",
+        /// where "name" is the department name and "subject" is the subject abbreviation.
+        /// </summary>
+        /// <returns>The JSON array</returns>
+        public IActionResult GetDepartments()
+        {
+            var departments =
+                from department in db.Departments
+                select new { name = department.Name, subject = department.Subject };
+
+            return Json(departments.ToArray());
+        }
+
+
+
+        /// <summary>
+        /// Returns a JSON array representing the course catalog.
+        /// Each object in the array should have the following fields:
+        /// "subject": The subject abbreviation, (e.g. "CS")
+        /// "dname": The department name, as in "Computer Science"
+        /// "courses": An array of JSON objects representing the courses in the department.
+        ///            Each field in this inner-array should have the following fields:
+        ///            "number": The course number (e.g. 5530)
+        ///            "cname": The course name (e.g. "Database Systems")
+        /// </summary>
+        /// <returns>The JSON array</returns>
+        public IActionResult GetCatalog()
+        {
+            var courseCatalog =
+                from department in db.Departments
+                join courses in (
+                    from c in db.Courses
+                    group c by c.Department into courseGroup
+                    select new { Department = courseGroup.Key, Courses = courseGroup.Select(c => new { number = c.Number, cname = c.Name }).ToArray() }
+                ) on department.Subject equals courses.Department
+                select new
+                {
+                    subject = department.Subject,
+                    dname = department.Name,
+                    courses = courses.Courses
+                };
+
+
+            return Json(courseCatalog.ToArray());
+        }
+
+        /// <summary>
+        /// Returns a JSON array of all class offerings of a specific course.
+        /// Each object in the array should have the following fields:
+        /// "season": the season part of the semester, such as "Fall"
+        /// "year": the year part of the semester
+        /// "location": the location of the class
+        /// "start": the start time in format "hh:mm:ss"
+        /// "end": the end time in format "hh:mm:ss"
+        /// "fname": the first name of the professor
+        /// "lname": the last name of the professor
+        /// </summary>
+        /// <param name="subject">The subject abbreviation, as in "CS"</param>
+        /// <param name="number">The course number, as in 5530</param>
+        /// <returns>The JSON array</returns>
+        public IActionResult GetClassOfferings(string subject, int number)
+        {
+            var classOfferings =
+                from course in db.Courses
+                where course.Department == subject && course.Number == number
+                join classes in db.Classes on course.CatalogId equals classes.Listing into allOfferings
+                from z in allOfferings
+                join professor in db.Professors on z.TaughtBy equals professor.UId into professor
+                from x in professor.DefaultIfEmpty()
+                select new { season = z.Season, year = z.Year, location = z.Location, start = z.StartTime, end = z.EndTime, fname = x.FName == null ? "" : x.FName, lname = x.LName == null ? "" : x.LName };
+      
+
+            return Json(classOfferings.ToArray());
+        }
+
+        /// <summary>
+        /// This method does NOT return JSON. It returns plain text (containing html).
+        /// Use "return Content(...)" to return plain text.
+        /// Returns the contents of an assignment.
+        /// </summary>
+        /// <param name="subject">The course subject abbreviation</param>
+        /// <param name="num">The course number</param>
+        /// <param name="season">The season part of the semester for the class the assignment belongs to</param>
+        /// <param name="year">The year part of the semester for the class the assignment belongs to</param>
+        /// <param name="category">The name of the assignment category in the class</param>
+        /// <param name="asgname">The name of the assignment in the category</param>
+        /// <returns>The assignment contents</returns>
+        public IActionResult GetAssignmentContents(string subject, int num, string season, int year, string category, string asgname)
+        {
+            var getAssignment =
+                (from course in db.Courses
+                where course.Department == subject && course.Number == num
+                join classes in db.Classes on course.CatalogId equals classes.Listing
+                from assignCats in db.AssignmentCategories
+                where classes.ClassId == assignCats.InClass
+                join assignment in db.Assignments on assignCats.CategoryId equals assignment.Category
+                where assignment.Name == asgname
+                select assignment.Contents).First();
+                
+               
+            return Content(getAssignment);
+        }
+
+
+        /// <summary>
+        /// This method does NOT return JSON. It returns plain text (containing html).
+        /// Use "return Content(...)" to return plain text.
+        /// Returns the contents of an assignment submission.
+        /// Returns the empty string ("") if there is no submission.
+        /// </summary>
+        /// <param name="subject">The course subject abbreviation</param>
+        /// <param name="num">The course number</param>
+        /// <param name="season">The season part of the semester for the class the assignment belongs to</param>
+        /// <param name="year">The year part of the semester for the class the assignment belongs to</param>
+        /// <param name="category">The name of the assignment category in the class</param>
+        /// <param name="asgname">The name of the assignment in the category</param>
+        /// <param name="uid">The uid of the student who submitted it</param>
+        /// <returns>The submission text</returns>
+        public IActionResult GetSubmissionText(string subject, int num, string season, int year, string category, string asgname, string uid)
+        {
+            var getSubmission =
+                (from course in db.Courses
+                 where course.Department == subject && course.Number == num
+                 join classes in db.Classes on course.CatalogId equals classes.Listing
+                 from assignCats in db.AssignmentCategories
+                 where classes.ClassId == assignCats.InClass
+                 join assignment in db.Assignments on assignCats.CategoryId equals assignment.Category
+                 where assignment.Name == asgname
+                 join submission in db.Submissions on assignment.AssignmentId equals submission.Assignment
+                 where submission.Student == uid
+                 select submission.SubmissionContents).FirstOrDefault();
+
+            if(getSubmission == null)
+            {
+                return Content("");
+            }
+            else
+            {
+                return Content(getSubmission);
+            }
+        }
+
+
+        /// <summary>
+        /// Gets information about a user as a single JSON object.
+        /// The object should have the following fields:
+        /// "fname": the user's first name
+        /// "lname": the user's last name
+        /// "uid": the user's uid
+        /// "department": (professors and students only) the name (such as "Computer Science") of the department for the user. 
+        ///               If the user is a Professor, this is the department they work in.
+        ///               If the user is a Student, this is the department they major in.    
+        ///               If the user is an Administrator, this field is not present in the returned JSON
+        /// </summary>
+        /// <param name="uid">The ID of the user</param>
+        /// <returns>
+        /// The user JSON object 
+        /// or an object containing {success: false} if the user doesn't exist
+        /// </returns>
+        public IActionResult GetUser(string uid)
+        {
+            //check if they are a professor
+            var professor =
+                (from professors in db.Professors
+                where professors.UId == uid
+                join department in db.Departments on professors.WorksIn equals department.Subject
+                select new { fname = professors.FName, lname = professors.LName, uid = professors.UId, department = professors.WorksIn }).FirstOrDefault();
+
+            if (professor != null)
+            {
+                return Json(professor);
+            }
+
+            //check if the are a student
+            var student =
+                (from students in db.Students
+                where students.UId == uid
+                join department in db.Departments on students.Major equals department.Subject
+                select new { fname = students.FName, lname = students.LName, uid = students.UId, department = department.Name }).FirstOrDefault();
+
+            if (student != null)
+            {
+                return Json(student);
+            }
+
+            //check if they are an admin
+            var admin =
+                (from admins in db.Administrators
+                where admins.UId == uid
+                select new { fname = admins.FName, lname = admins.LName, uid = admins.UId }).FirstOrDefault();            
+            
+            if (admin != null) 
+            {
+                return Json(admin);
+            }
+            else 
+            { 
+                return Json(new { success = false });
+            }
+        }
+        /*******End code to modify********/
+    }
+}
+
